823f4c4e0d3e1f8f47085e943717d60fc83aff06
Le Minh Quan <leminhquan270305@gmail.com>
2026-02-11 10:20:12 +0700

    layout: create general home page for user

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import { AuthForm } from "@/components/auth/AuthForm";
+
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 px-4 dark:bg-black">
+      <AuthForm mode="login" />
+    </div>
+  );
+}
+
